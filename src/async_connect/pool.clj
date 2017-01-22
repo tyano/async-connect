903dc102c1e6983232aaac04740088deffe42d33
Tsutomu YANO <t_yano@me.com>
2017-01-22 20:17:39 +0900

    Added `pool` ns for supporting connection-pooling.

---
+++ src/async_connect/pool.clj
@@ -0,0 +1,90 @@
+(ns async-connect.pool
+  (:require [async-connect.client :refer [IConnection] :as client]
+            [clojure.spec :as s]
+            [clojure.tools.logging :as log]
+            [clojure.core.async :refer [thread chan <! >! go-loop]]
+            [async-connect.netty :refer [bytebuf->string
+                                         string->bytebuf]])
+  (:import [io.netty.channel.socket
+              SocketChannel]
+           [io.netty.channel
+              ChannelFutureListener]
+           [io.netty.bootstrap
+              Bootstrap]))
+
+(s/def :pool/host string?)
+(s/def :pool/port pos-int?)
+(s/def :pool/key
+  (s/keys :req [:pool/host :pool/port]))
+
+(def ^:private pooled-connections (volatile! {}))
+
+(defn remove-from-pool
+  [{:keys [:pool/host :pool/port] :as conn}]
+  (let [pool-key {:pool/host host, :pool/port port}]
+    (locking pooled-connections
+      (log/debug "removing a connection:" conn)
+      (vswap! pooled-connections update pool-key #(when % (vec (filter (fn [c] (not= c conn)) %)))))
+    nil))
+
+(defrecord PooledConnection []
+  IConnection
+  (close
+    [{:keys [:pool/host :pool/port] :as conn}]
+    (let [pool-key {:pool/host host, :pool/port port}]
+      (locking pooled-connections
+        (log/debug "returning a connection:" conn)
+        (vswap! pooled-connections update pool-key #(if % [conn] (conj % conn))))
+      nil)))
+
+(defn connect
+  "Connect to a `port` of a `host` using `bootstrap`, and return a IConnection object, but before making
+   a new real connection, this fn checks a pool containing already connected connections and if the pool
+   have a connection with same address and port, this fn don't make a new connection but return the found
+   connection.
+   If read-ch and write-ch are supplied, all data written and read are transfered to the supplied channels,
+   If read-ch and write-ch aren't supplied, channels made by `(chan)` are used."
+  ([^Bootstrap bootstrap ^String host port read-ch write-ch]
+    (let [pool-key {:pool/host host, :pool/port port}]
+      (locking pooled-connections
+        (let [conns (get @pooled-connections pool-key)
+              found (first conns)]
+          (vswap! pooled-connections update pool-key (fn [_] (vec (rest conns))))
+          (if found
+            (do
+              (log/debug "a pooled connection is found: " found)
+              found)
+            (do
+              (log/debug "no pooled connection is found. create a new one.")
+              (let [{:keys [:client/channel] :as new-conn}
+                        (merge (->PooledConnection)
+                              (client/connect bootstrap host port read-ch write-ch)
+                              {:pool/host host
+                               :pool/port port})]
+
+                ;; remove this new-conn from our connection-pool when this channel is closed.
+                (thread
+                  (.. ^SocketChannel channel
+                    (closeFuture)
+                    (addListener
+                      (reify ChannelFutureListener
+                        (operationComplete [this f]
+                          (remove-from-pool new-conn))))
+                    (sync)))
+
+                new-conn)))))))
+
+  ([^Bootstrap bootstrap host port]
+    (connect bootstrap host port nil nil)))
+
+
+(defn sample-connect
+  []
+  (let [bootstrap (client/make-bootstrap {})
+        read-ch  (chan 1 bytebuf->string)
+        write-ch (chan 1 string->bytebuf)
+        conn     (connect bootstrap "localhost" 8080 read-ch write-ch)]
+    (go-loop []
+      (println "result: " @(<! read-ch))
+      (recur))
+    conn))
