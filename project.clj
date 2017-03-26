cf3b3a8da1a542ec9273e7a4d8202d2b6ec152d5
Tsutomu YANO <t_yano@me.com>
2017-03-26 18:40:15 +0900

    library version-up.

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://github.com/tyano/async-connect"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
                  [io.netty/netty-all "4.1.6.Final"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.clojure/core.async "0.2.395"]]
+                 [org.clojure/core.async "0.3.442"]]
 
   :profiles {:dev
-              {:dependencies [[ch.qos.logback/logback-classic "1.1.8"]
+              {:dependencies [[ch.qos.logback/logback-classic "1.2.2"]
                               [org.clojure/tools.namespace "0.2.11"]
                               [org.clojure/test.check "0.9.0"]]
                :resource-paths ["resources-dev"]}
--- test/async_connect/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns async-connect.core-test
-  (:require [clojure.test :refer :all]
-            [async-connect.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
