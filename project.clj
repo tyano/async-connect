e851512ddce5fd8408a68690679f31c70aaa433a
Tsutomu YANO <t_yano@me.com>
2018-03-13 16:23:27 +0900

    Netty 4.1.22.Final

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [io.netty/netty-all "4.1.21.Final"]
+                 [io.netty/netty-all "4.1.22.Final"]
                  [org.clojure/tools.logging "0.4.0"]
                  [org.clojure/core.async "0.4.474"]]
 
